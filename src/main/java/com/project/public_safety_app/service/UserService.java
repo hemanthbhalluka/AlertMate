887ede1e2778ac84dc2dba77ae028ccf88ec6b1a
hemanthbhalluka <hemanthbhalluka@dev.local>
2026-07-16 16:52:47 +0530

    UserService.java

--- src/main/java/com/project/public_safety_app/service/UserService.java
+++ src/main/java/com/project/public_safety_app/service/UserService.java
@@ -3,6 +3,7 @@ package com.project.public_safety_app.service;
 import com.project.public_safety_app.dto.EmergencyContactDto;
 import com.project.public_safety_app.dto.UserDto;
 import com.project.public_safety_app.dto.UserResponse;
+import org.springframework.security.access.prepost.PreAuthorize;
 
 import java.util.List;
 
@@ -10,13 +11,16 @@ public interface UserService {
     UserDto createUser(UserDto userdto);
     UserDto login(String userName, String password);
     UserDto updateUser(Long userId, UserDto userDto);
+
+    @PreAuthorize("hasRole('ROLE_ADMIN') or #userId == authentication.principal.id")
     void deleteUser(Long userId);
+
     UserDto getUserByName(String name);
+
+    @PreAuthorize("hasRole('ROLE_ADMIN') or #name == authentication.principal.username")
     void deleteUserByName(String name);
+
     List<EmergencyContactDto> getUserContactsByUsername(String username);
     List<UserResponse> getAllUsers();
     UserResponse getUser(String userName);
-
-
-
 }
